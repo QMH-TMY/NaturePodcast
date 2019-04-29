22c36df7ae6cb54d2092cbedbabb4258e9b3223e
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-04-29 16:16:26 +0800

    add a shell script

---
+++ trans2pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 批量转换当前目录下txt为同名pdf
+
+for file in `ls $1`;do 
+	if [ "${file##*.}" = "txt" ]
+	then
+		pandoc -t latex -o "$1${file%.*}.pdf" $1${file} 2> error.txt
+		/bin/rm $1${file} #删除.txt文件
+	fi
+done
